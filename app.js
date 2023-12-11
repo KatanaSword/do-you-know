2e464591a094e71cdfeb42150a35e05ba6ddb761
KatanaSword <114555620+KatanaSword@users.noreply.github.com>
2023-12-11 15:22:27 +0530

    refactor - update app.js

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ import readlineSync from "readline-sync";
 import chalk from "chalk";
 import { scientificFacts, historicalFacts, culturalFacts, socialFacts } from "./database.js";
 const log = console.log;
-
 let userTopic;
 
 function greeting() {
-    log(chalk.blue.bold.italic("🌟 Welcome to 'Do You Know?'🌟\nUnlock a world of fascinating facts! Ready for a journey of discovery?"))
+    log(chalk.blue.bold.italic("🌟 Welcome to 'Do You Know?'🌟\nUnlock a world of fascinating facts! Ready for a journey of discovery?"));
 };
 greeting()
 
@@ -23,10 +22,10 @@ function userNameInput() {
 userNameInput()
 
 function selectTopic() {
-    log(chalk.bold.underline("Select topic"))
+    log(chalk.bold.underline("Select topic"));
     log(`${chalk.green.bold("Science")} ${chalk.yellow.bold("History")} ${chalk.blueBright.bold("Culture")} ${chalk.cyanBright.bold("Social")}`);
     log(`${chalk.bgGray.bold("Press key:")} ${chalk.bold("Science: s History: h Culture: c Social: ss")}`);
-    userTopic = readlineSync.question(chalk.cyan.bold("Please enter your topic: "))
+    userTopic = readlineSync.question(chalk.cyan.bold("Please enter your topic: "));
     displayFact()
 };
 
@@ -34,28 +33,28 @@ function displayFact() {
     if(userTopic === "s") {
         log(chalk.green.bold.underline("Did you know?"));
         const currentIndex = Math.floor(Math.random() * scientificFacts.length);
-        log(chalk.green.bold(scientificFacts[currentIndex].fact))
+        log(chalk.green.bold(scientificFacts[currentIndex].fact));
     }
     else if(userTopic === "h") {
         log(chalk.yellow.bold.underline("Did you know?"));
         const currentIndex = Math.floor(Math.random() * historicalFacts.length);
-        log(chalk.yellow.bold(historicalFacts[currentIndex].fact))
+        log(chalk.yellow.bold(historicalFacts[currentIndex].fact));
     }
     else if(userTopic === "c") {
         log(chalk.blueBright.bold.underline("Did you know?"));
         const currentIndex = Math.floor(Math.random() * culturalFacts.length);
-        log(chalk.blueBright.bold(culturalFacts[currentIndex].fact))
+        log(chalk.blueBright.bold(culturalFacts[currentIndex].fact));
     }
     else if(userTopic === "ss") {
         log(chalk.cyanBright.bold.underline("Did you know?"));
         const currentIndex = Math.floor(Math.random() * socialFacts.length);
-        log(chalk.cyanBright.bold(socialFacts[currentIndex].fact))
+        log(chalk.cyanBright.bold(socialFacts[currentIndex].fact));
     } else {
-        log(chalk.bgRed.bold("Error occured, Please enter your topic"))
+        log(chalk.bgRed.bold("Error occured, Please enter your topic"));
         selectTopic()
     };
     changeTopicOrExitOrContinueFunc()
-}
+};
 
 function changeTopicOrExitOrContinueFunc() {
     const changeTopicOrExitOrContinue = readlineSync.question(`${chalk.magenta.bold.underline("Do you want to change topic or continue or exit [x/y/n]")}${chalk.magenta.bold(":")} `);
@@ -66,11 +65,11 @@ function changeTopicOrExitOrContinueFunc() {
     } else if(changeTopicOrExitOrContinue === "n") {
         exit()
     } else {
-        log(chalk.bgRed.bold("Error occured, Please enter [x/y/n] to continue"))
+        log(chalk.bgRed.bold("Error occured, Please enter [x/y/n] to continue"));
         changeTopicOrExitOrContinueFunc()
     };
 };
 
 function exit() {
-    log(`${"👋"} ${chalk.blue.bold.italic("Thank you for exploring 'Do You Know?'\n!Feel free to return anytime for more fascinating facts. Happy learning!")}`)
+    log(`${"👋"} ${chalk.blue.bold.italic("Thank you for exploring 'Do You Know?'\n!Feel free to return anytime for more fascinating facts. Happy learning!")}`);
 };
